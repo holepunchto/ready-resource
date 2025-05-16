7c4027be5e2843808d42afc3df0c013025149ec1
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-05-16 18:00:41 +0200

    Rm long comment

--- index.js
+++ index.js
@@ -11,16 +11,6 @@ module.exports = class ReadyResource extends EventEmitter {
     this.opened = false
     this.closed = false
 
-    // State machine:
-    // - We track the desired state at any time (in shouldBeSuspended)
-    // - suspend/resume calls check if their desiredState matches the current state (in suspended)
-    //   - Note: 'suspended' means that it is either suspending, or currently suspended (we do not track 'suspending' or 'resuming' state)
-    // - if not:
-    //   - wait for mutex
-    //   - check if the desired state still matches what they want.
-    //     - If so: run suspend/resume logic
-    // => _suspend/_resume only start running if it's the current desired state AND we're not in it
-    // => _suspend/_resume are guaranteed to run if a state change is needed
     this.shouldBeSuspended = suspended
     this.suspended = suspended
     this._suspendMutex = mutexify()
